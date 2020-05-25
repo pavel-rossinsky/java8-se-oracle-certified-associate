b7b93fe9cc045bcd65a9ad0ed90d24d6c27b2e7f
prossinsky <po.rossinsky@gmail.com>
2020-05-25 22:25:57 +0200

    Complete the Q4 class

--- src/main/java/com/example/app/c1/Q4.java
+++ src/main/java/com/example/app/c1/Q4.java
@@ -5,6 +5,6 @@ import java.sql.*;
 
 // Compilation error: reference to Date is ambiguous
 public class Q4 {
-    private Date rob = new Date();
+    // private Date rob = new Date();
     private java.util.Date sharon = new java.util.Date();
 }
